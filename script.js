58ccae5f4b74d06c84faa2d5d4cd2d14831a79a9
Prabin <prabinkumarsahu2001@gmail.com>
2023-06-06 21:42:52 +0530

    sayancommit

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+// const API_KEY="3ab48aae71b0439c849a0e8a555217ad";
+// const url="https://newsapi.org/v2/everything?q=";
+
+// window.addEventListener("load", () => fetchNews("India"));
+
+// async function fetchNews (query)
+// {
+//  const res = await fetch (`${url}${query}&apiKey=${API_KEY}`);
+//  const data=await res.json();
+//  bindData(data.articles);
+
+
+// }
+// function bindData(articles)
+// {
+//     const cardscontainer=document.getElementById("cards-container");
+//     const newsCardTemplate =document.getElementById("template-news-card");  
+
+//     cardscontainer.innerHTML="";
+    
+//     articles.forEach((article)=>{
+//         if(!articles.urlToImage)return;
+//         const cardClone=newsCardTemplate.content.cloneNode(true);
+//         fillDataInCard(cardClone , article);
+//         cardscontainer.appendChild(cardClone);
+//     });
+// }
+//     function fillDataInCard(cardClone , article)
+//     {
+//         const newsImg =cardClone.querySelector('#news-img');
+//         const newsTitle =cardClone.querySelector('#news-title');
+//         const newsSource =cardClone.querySelector('#news-source');
+//         const newsDesc =cardClone.querySelector('#news-desc');
+
+//         newsImg.src=article.urlToImage;
+//         newsTitle.innerHTML=article.title;
+//         newsDesc.innerHTML=article.description;
+
+//         const date=new Date(article.publishAt).toLocaleString("en-US",
+//         {
+//             timeZone:"Asia/Jakarta"
+//         });
+
+
+//         newsSource.innerHTML=`${article.source.name} . ${date}`;
+//     }
+
 
 
 
@@ -12,38 +59,18 @@ async function fetchNews(query) {
   bindData(data.articles);
 }
 
-// function bindData(articles) {
-//   const cardscontainer = document.getElementById("card-container");
-//   const newsCardTemplate = document.getElementById("template-news-card");
-
-//   cardscontainer.innerHTML = "";
-
-//   articles.forEach((article) => {
-//     if (!article.urlToImage) return;
-//     const cardClone = newsCardTemplate.content.cloneNode(true);
-//     fillDataInCard(cardClone, article);
-//     cardscontainer.appendChild(cardClone);
-//   });
-// }
 function bindData(articles) {
   const cardscontainer = document.getElementById("card-container");
   const newsCardTemplate = document.getElementById("template-news-card");
 
   cardscontainer.innerHTML = "";
 
-  if (articles && articles.length > 0) {
-    articles.forEach((article) => {
-      if (!article.urlToImage) return;
-      const cardClone = newsCardTemplate.content.cloneNode(true);
-      fillDataInCard(cardClone, article);
-      cardscontainer.appendChild(cardClone);
-    });
-  } else {
-    // Handle the case when articles is undefined or empty
-    const errorMessage = document.createElement("p");
-    errorMessage.textContent = "No articles found.";
-    cardscontainer.appendChild(errorMessage);
-  }
+  articles.forEach((article) => {
+    if (!article.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, article);
+    cardscontainer.appendChild(cardClone);
+  });
 }
 
 function fillDataInCard(cardClone, article) {
@@ -110,6 +137,3 @@ function performSearch() {
   {
     window.location.reload();
   }
-
-
-
